5e1a94964602daf845885a83dd39815dfb0cd1cf
Matatrolls <pro.nhk90@gmail.com>
2023-08-15 07:55:46 -0500

    AAA

---
+++ TrabajosClase/Seguimiento2/src/model/Node.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Node {
+    
+    private int key;
+
+    private Node next;
+
+    public Node(int key) {
+        this.key = key;
+    }
+
+    /**
+     * @return the key
+     */
+    public int getKey() {
+        return key;
+    }
+
+    /**
+     * @param key the key to set
+     */
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    /**
+     * @return the next
+     */
+    public Node getNext() {
+        return next;
+    }
+
+    /**
+     * @param next the next to set
+     */
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public String toString(){
+        return key+"";
+    }
+}
